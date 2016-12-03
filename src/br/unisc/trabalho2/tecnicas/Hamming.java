a047e7be290549a7e19b45a8d35d69946982a5bb
Guilherme Seibert <guiseibert@idealogic.com.br>
2016-12-03 16:30:57 -0200

    sd

--- src/br/unisc/trabalho2/tecnicas/Hamming.java
+++ src/br/unisc/trabalho2/tecnicas/Hamming.java
@@ -5,23 +5,61 @@
  */
 package br.unisc.trabalho2.tecnicas;
 
+import java.util.Vector;
+
 /**
  *
  * @author Guilherme
  */
 public class Hamming {
 
-    public int potenciadeDois(int numero) {
-        System.out.println("numero:"+numero);
-        
-        return numero / 2;
+    int bit;
+    String indice;
+
+    public int getBit() {
+        return bit;
     }
-    
-    public void  calcularHamming (String array[]){
+
+    public void setBit(int bit) {
+        this.bit = bit;
+    }
+
+    public String getIndice() {
+        return indice;
+    }
+
+    public void setIndice(String indice) {
+        this.indice = indice;
+    }
+
+    Vector arrayPotencais;
+    int valor = 0;
+    int x = 0;
+
+    //calcula as potencais de 2 // recursivo
+    public int potenciadeDois(int numero) {
+        System.out.println("numero:" + numero);
+        arrayPotencais.add(numero);
+        if (numero == 1) {
+            return 1;
+        }
+        x++;
+        int aux = numero / 2;
+        return potenciadeDois(aux);
+    }
+
+    public void calcularHamming(String array[]) {
         int numeroBits = array.length;
-        for (int i = 1; i <= numeroBits; i++) {
+        arrayPotencais = new Vector();
+        // encontra as potencias e armazena na arrayPotencais
+        potenciadeDois(numeroBits);
+        
+        int aux = numeroBits+arrayPotencais.size();
+        
+        for (int i = 0; 1 < aux; i++) {
             
         }
+
     }
 
 }
